6b14ac0250937a803f28cca2bd50f9bc6289a1a1
CristhianRuiz593 <cristhian.ruiz.gamboa@hotmail.com>
2015-11-29 10:32:09 -0500

    Ejercicia

---
+++ Salazar/src/Deberes/Ejercicio1.java
@@ -0,0 +1,12 @@
+package Deberes;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Circulo cir1=new Circulo();
+		cir1.setRadio(10);
+        System.out.println("Area cr1:"+cir1.getArea());
+	}
+
+}
